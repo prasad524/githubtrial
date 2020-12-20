07da0909e3e3c2a571bdb9f24d9aedc30cf91d93
LENOVO <LENOVO@DESKTOP-IF1RFIU>
2020-12-20 23:07:57 +0530

    hvjmj

---
+++ Log1/src/Avgofarray.java
@@ -0,0 +1,47 @@
+
+public class Avgofarray {
+	
+
+	public static void main(String[] args) {
+		
+		
+		int a[]= {1,9,4,6,85,35};
+		int sum=0;
+		double avg=0;
+		for(int i=0; i<=a.length-1; i++)
+		{
+			sum= sum+a[i];
+			avg= sum/2;
+		}
+		System.out.println(avg);
+		
+		
+		System.out.println("finding specific number and removing that number from array");
+		
+		for(int i=0; i<=a.length-1; i++)
+		{
+			if(a[i]==85)
+			{
+				System.out.println(i);
+				
+			}
+			else
+			{
+				for(int j=0; j<=a.length-1; j++)
+				{
+					a[i]=a[j];
+				System.out.println(a[j]);
+				
+			}
+		}
+		}
+		
+		
+		
+	
+		
+		
+	}
+	
+
+}
